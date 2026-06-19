55876ba19711ffde2cda543aad4889d86d80bdc6
Multigriffes <jonas.t@laposte.net>
2026-06-19 23:36:31 +0200

    2015 Day 3 Done

---
+++ README.md
@@ -0,0 +1,4 @@
+## Things I learned
+- If you want to get an element in a `vec` with a variable index, the index needs to be `usize`
+- You can pass argument to the binarie ran by `cargo run` with `-- --option`
+- more to come...
---
+++ solutions/year_2015/src/bin/day3.rs
@@ -0,0 +1,80 @@
+use std::{collections::HashMap, env, error::Error};
+
+use input_file_lib::get_file_content_to_string;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    if (args.len() < 2) | (args.len() > 3) {
+        println!("Usage: day3 [option] <input_file>");
+        println!("Options: -r for robot santa");
+	    std::process::exit(1);
+    }
+
+    let robot_santa: bool;
+    let file_path_index: usize;// usize car le compilateur ne sait pas si une taille précise (u8) pourra être utilisé dans le vecteur dans la mesure où sa taille dépant du système
+    match args[1].as_str() {
+        "-r" => {
+            robot_santa = true;
+            file_path_index = 2;
+        },
+        _ => {
+            robot_santa = false;
+            file_path_index = 1;
+        }
+    }
+    
+    // usize car le compilateur ne sait pas si une taille précise (u8) pourra être utilisé dans le vecteur dans la mesure où sa taille dépant du système
+    let string_to_compute = get_file_content_to_string(&args[file_path_index])?;
+    let mut house_list: HashMap<[i32; 2], u8> = HashMap::new();
+
+
+    let (mut x_santa, mut y_santa, mut x_robot, mut y_robot) = (0, 0, 0, 0);
+    house_list.insert([0, 0], 1);
+    let mut counter = 0;
+    for char in string_to_compute.chars() {
+        match char {
+            '^' => {
+                if robot_santa & ((counter % 2) == 1) {
+                    y_robot += 1;
+                    house_list.entry([x_robot, y_robot]).and_modify(|x| *x += 1).or_insert(1);
+                } else {
+                    y_santa += 1;
+                    house_list.entry([x_santa, y_santa]).and_modify(|x| *x += 1).or_insert(1);
+                }
+            },
+            'v' => {
+                if robot_santa & ((counter % 2) == 1) {
+                    y_robot -= 1;
+                    house_list.entry([x_robot, y_robot]).and_modify(|x| *x += 1).or_insert(1);
+                } else {
+                    y_santa -= 1;
+                    house_list.entry([x_santa, y_santa]).and_modify(|x| *x += 1).or_insert(1);
+                }
+            },
+            '<' => {
+                if robot_santa & ((counter % 2) == 1) {
+                    x_robot -= 1;
+                    house_list.entry([x_robot, y_robot]).and_modify(|x| *x += 1).or_insert(1);
+                } else {
+                    x_santa -= 1;
+                    house_list.entry([x_santa, y_santa]).and_modify(|x| *x += 1).or_insert(1);
+                }
+            },
+            '>' => {
+                if robot_santa & ((counter % 2) == 1) {
+                    x_robot += 1;
+                    house_list.entry([x_robot, y_robot]).and_modify(|x| *x += 1).or_insert(1);
+                } else {
+                    x_santa += 1;
+                    house_list.entry([x_santa, y_santa]).and_modify(|x| *x += 1).or_insert(1);
+                }
+            },
+            _ => {continue;}
+        }
+        counter += 1;
+    }
+
+    println!("{} house with at least one present", house_list.len());
+
+    Ok(())
+}
