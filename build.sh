7a21ed48b04a5ef8e2d373d3d9ee67c8c9bdee2b
Multigriffes <jonas.t@laposte.net>
2026-06-27 17:03:27 +0200

    Update workflow

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/bash 
+#!/usr/bin/bash
 
 for folder in $(find ./solutions/ -maxdepth 1 -mindepth 1 -type d); do
     folder=${folder##*/}
     if [ "$folder" != "year_X" ]; then
-        #echo "$folder"
+        echo "╔═ $folder ═"
         export CARGO_TARGET_DIR=./build/"$folder"
         #echo "$CARGO_TARGET_DIR"
-        cargo build -r -p "$folder";
+        cargo build -r -p "$folder"
+        echo "╚═ $folder ═"
     fi
 done
